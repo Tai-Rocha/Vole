709dc6ec22dfa93e12b27afdd312a42694865a00
Tai-Rocha <taina013@gmail.com>
2020-04-15 17:21:35 -0300

    updated but not work well

--- Rscripts/Post-processing analysis.R
+++ Rscripts/Post-processing analysis.R
@@ -3,28 +3,15 @@
 ## Post-processing analysis for Microtus californicus ENMs 
 ## Author: Tainá Rocha
 ## DAte: 07 April 2020
-## Last Update: 10 April 2020 
+## Last Update: 15 April 2020 
 ###############################################################
 
 ### Library pckgs
+
 library(raster)
 library(rgdal)
 
-
-########################################################### Loops
-
-## List raw mean raster final models (5 algorithms) and stack (out all layers together)
-
-list_all <-list.files("./Vole_raw_mean_Present/All_vole/Present/final_models/raw_mean_models/", pattern = "tif", full.names = T)
-
-stack_all <- stack(list_all)
-plot(stack_all)
-
-
-
-
-
-
+############################################# Loops
 
 setwd("Vole_raw_mean_Present/")
 
@@ -37,10 +24,11 @@ for(j in 1:length(sub)) {
   print(h)
   
   stack_present <- stack(h)
+  print(stack_present)
   
   binary_0.2 <- stack_present >=0.2
   
-  writeRaster(binary_0.2, filename=paste0(sub[j], bylayer = T, suffix = "_bin.tif"), overwrite=TRUE)
+  writeRaster(binario, filename=paste0(sub[j], bylayer = T, suffix = "_bin.tif"), overwrite=TRUE)
   
 }
 #bin <- unstack(binary_0.2)
@@ -49,4 +37,3 @@ for(j in 1:length(sub)) {
 #for(i in seq_along(bin)){writeRaster(bin[[i]], overwrite=TRUE, file=binary[i]) }
 
 #binary <- stackApply(h, indices =  rep(1,nlayers(h)), fun = "mean", na.rm = T)
-
