1988beb141e245dba57dde771750398f3ca63d9a
Tai-Rocha <taina013@gmail.com>
2020-04-16 20:12:28 -0300

    in progress again

--- Rscripts/Post-processing analysis.R
+++ Rscripts/Post-processing analysis.R
@@ -1,4 +1,3 @@
-
 ###############################################################
 ## Post-processing analysis for Microtus californicus ENMs 
 ## Author: Tainá Rocha
@@ -11,9 +10,7 @@
 library(raster)
 library(rgdal)
 
-
-############################################# Loops
-
+########## Present 
 setwd("Vole_raw_mean_Present/")
 
 sub <- list.dirs(full.names=TRUE, recursive=FALSE)
@@ -29,24 +26,16 @@ for(j in 1:length(sub)) {
   
   binary_0.2 <- stack_present >=0.2
   
-  f <- paste0(names(binary_0.2@data),"_bin.tif")
+  b <- paste0(names(binary_0.2@data),"_bin.tif")
+
+  writeRaster(binary_0.2, filename=b, bylayer=TRUE, overwrite=TRUE)
   
+############ DAqui pra baixo não funciona
   
-  # f <-paste0(sub[j],"/",strsplit(sub[j],"/")[[1]][2], "_bin.tif") 
-  # f <- paste0(names(binary_0.2@data), "bin.tif") # Work well
-  # f <- paste0(names(binary_0.2@data),strsplit(sub[j],"/")[[1]][2], "_bin.tif") # Work well with right names but still save in the same folder
-  # f <- paste0(sub[j], "bin.tif") # Work well but save all in the same folder
-  # writeRaster(binary_0.2, filename=f, bylayer=TRUE, overwrite=TRUE)
+  continu <- binary_0.2*stack_present
   
-  writeRaster(binary_0.2, filename=f, paste0(sub[j],"/",strsplit(sub[j],"/")[[1]][2], ".tif"), overwrite=TRUE,  bylayer=TRUE)
+  c <- paste0(names(binary_0.2@data),"_cont.tif")
+  writeRaster(continu, filename=c, bylayer=TRUE, overwrite=TRUE)
+ 
 }
-  ####################################################
-
-setwd("Vole_raw_mean_Present/")
-
-dir <- list.dirs(recursive=FALSE)
-
-for(j in 1:length(dir)){
-  writeRaster(stack( list.files(path=dir[j], recursive=TRUE, full.names=TRUE, pattern='rain'))*2,
-              paste0(dir[j],"/",strsplit(dir[j],"/")[[1]][2], "_new.tif"), overwrite=TRUE,  bylayer=TRUE )
-}
+##########  End
